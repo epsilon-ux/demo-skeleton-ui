421b3df35ede3999cdc36bc9dde2c77477a854c1
Ash Johns <ashleypjohns@gmail.com>
2020-09-23 16:38:23 -0400

    correct packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ConversantUIKitModule } from '@cnvr/cuik';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ConversantUIKitModule
   ],
   providers: [],
   bootstrap: [AppComponent]
